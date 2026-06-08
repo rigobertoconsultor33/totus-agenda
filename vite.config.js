f213f4fce4e536166cd41dca21d98b60a1ba05e8
rigobertoconsultor33 <rigobertoconsultor33@gmail.com>
2026-06-08 20:14:25 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/totus-agenda/",
+  build: {
+    rollupOptions: {
+      input: "./index.html"
+    }
+  }
 });
